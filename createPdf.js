f7c22bd023167b89fdb9a59400406e111fffa0e9
ogatalars <ogataogara@gmail.com>
2024-06-25 15:59:50 -0300

    first commit

---
+++ createPdf.js
@@ -0,0 +1,32 @@
+const { PDFDocument, rgb } = require("pdf-lib");
+const fs = require("fs");
+
+async function createPdf() {
+  const pdfDoc = await PDFDocument.create();
+  const page = pdfDoc.addPage([600, 400]);
+  const { width, height } = page.getSize();
+
+  const fontSize = 30;
+  const text = "Click here to visit the link";
+  const url = "https://ogatalars.github.io/my-cv"; // substitua pelo link desejado
+
+  page.drawText(text, {
+    x: 50,
+    y: height / 2 + fontSize / 2,
+    size: fontSize,
+    color: rgb(0, 0, 1),
+  });
+
+  page.drawText(url, {
+    x: 50,
+    y: height / 2 - fontSize,
+    size: fontSize,
+    color: rgb(0, 0, 1),
+  });
+
+  const pdfBytes = await pdfDoc.save();
+  fs.writeFileSync("link.pdf", pdfBytes);
+  console.log("PDF created!");
+}
+
+createPdf();
